623faad0a7e80e5887a7c02ac0e200ff13d5db01
Hazrtine <jamalvbadr@gmail.com>
2024-06-18 23:20:19 -0500

    For Funny Things

---
+++ src/main/java/org/osakabot/OsakaBot/commands/FunBot.java
@@ -0,0 +1,12 @@
+package org.osakabot.OsakaBot.commands;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class FunBot extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        event.getMessage()
+    }
+}
