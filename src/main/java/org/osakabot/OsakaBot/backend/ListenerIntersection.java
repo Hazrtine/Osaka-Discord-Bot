4b1695c02dd7589da15feedb3f3ea0ccc2c6b384
Hazrtine <jamalvbadr@gmail.com>
2024-06-18 23:20:27 -0500

    Working on it

--- src/main/java/org/osakabot/OsakaBot/backend/ListenerIntersection.java
+++ src/main/java/org/osakabot/OsakaBot/backend/ListenerIntersection.java
@@ -1,4 +1,22 @@
 package org.osakabot.OsakaBot.backend;
 
-public class ListenerIntersection {
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+
+public class ListenerIntersection extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        getAllIdentifiers();
+
+        String message = event.getMessage().getContentRaw();
+        if (message.charAt(0) == '!') {
+
+        }
+    }
+
+    private void getAllIdentifiers() {
+
+    }
 }
