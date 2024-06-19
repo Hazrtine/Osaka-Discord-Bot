623038032e0e55d4240bb2cc1c17cbf4b141e558
Hazrtine <jamalvbadr@gmail.com>
2024-06-19 00:22:56 -0500

    New Listener for Funny Happenings

--- src/main/java/org/osakabot/OsakaBot/commands/FunThings.java
+++ src/main/java/org/osakabot/OsakaBot/commands/FunThings.java
@@ -3,7 +3,8 @@ package org.osakabot.OsakaBot.commands;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class FunBot extends ListenerAdapter {
+public class FunThings extends ListenerAdapter {
+
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
