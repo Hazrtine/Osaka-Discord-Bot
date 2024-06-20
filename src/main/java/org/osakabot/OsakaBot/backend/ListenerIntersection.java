721a3bcfcb06088c0ee195162a8f9876e1237337
Hazrtine <jamalvbadr@gmail.com>
2024-06-20 13:51:44 -0500

    Optimizing Listeners

--- src/main/java/org/osakabot/OsakaBot/backend/ListenerIntersection.java
+++ src/main/java/org/osakabot/OsakaBot/backend/ListenerIntersection.java
@@ -2,21 +2,31 @@ package org.osakabot.OsakaBot.backend;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-
+import org.osakabot.OsakaBot.Osaka;
+import org.osakabot.OsakaBot.commands.FunThings;
+import org.osakabot.OsakaBot.commands.HelperBot;
+import org.osakabot.OsakaBot.commands.InformationBot;
+import org.osakabot.OsakaBot.commands.PlayBot;
 
 public class ListenerIntersection extends ListenerAdapter {
 
+    private static MessageReceivedEvent event;
+
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        getAllIdentifiers();
+    public void onMessageReceived(MessageReceivedEvent evente) {
+        event = evente;
+        MessagesToConsole(event);
+        String message =  event.getMessage().getContentRaw();
+        FunThings fT = new FunThings(event);
+        if (message.charAt(0) == '!') { //TODO: this sucks, fix this into something dynamic
+            if (message.contains("help"))
 
-        String message = event.getMessage().getContentRaw();
-        if (message.charAt(0) == '!') {
-
+            }
         }
+
+    private void MessagesToConsole(MessageReceivedEvent event) {
+        System.out.println(event.getMessage().getContentRaw() + "\nFrom: " + event.getAuthor().getName());
     }
 
-    private void getAllIdentifiers() {
-
-    }
 }
+
