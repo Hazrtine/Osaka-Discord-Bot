5d305689dc974aa5948653b4c92141cef9d49e67
Hazrtine <jamalvbadr@gmail.com>
2024-06-21 19:07:14 -0500

    Just something for the bot to respond to

--- src/main/java/org/osakabot/OsakaBot/commands/FunThings.java
+++ src/main/java/org/osakabot/OsakaBot/commands/FunThings.java
@@ -1,19 +1,19 @@
 package org.osakabot.OsakaBot.commands;
 
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class FunThings {
 
 
     public FunThings(MessageReceivedEvent event) {
         String message = event.getMessage().getContentRaw();
-        switch (message) {
-            case "h":
-                event.getChannel().sendMessage("h").queue();
-            case "i love this bot":
-                event.getChannel().sendMessage("https://tenor.com/view/azumanga-osaka-wave-hi-gif-26415274").queue();
-        }
+        if (!event.getAuthor().isBot())
+            switch (message) {
+                case "h":
+                    event.getChannel().sendMessage("h").queue();
+                    break;
+                case "i love this bot":
+                    event.getChannel().sendMessage("fellow knucklehead").queue();
+            }
     }
 }
