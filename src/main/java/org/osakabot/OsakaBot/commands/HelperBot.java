071d2643b7f0d82d947a72d080d324ee5c916493
Hazrtine <jamalvbadr@gmail.com>
2024-07-09 20:29:26 -0500

    i just wanna nap man

--- src/main/java/org/osakabot/OsakaBot/commands/HelperBot.java
+++ src/main/java/org/osakabot/OsakaBot/commands/HelperBot.java
@@ -1,24 +1,18 @@
 package org.osakabot.OsakaBot.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import org.osakabot.OsakaBot.Osaka;
 import org.osakabot.OsakaBot.backend.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.text.html.Option;
 import java.awt.*;
-import java.lang.reflect.Constructor;
 import java.util.List;
 
 public class HelperBot extends ListenerAdapter implements Command {
@@ -26,40 +20,42 @@ public class HelperBot extends ListenerAdapter implements Command {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        Guild guild = event.getGuild();
         TextChannel channel = event.getChannel().asTextChannel();
         Message message = event.getMessage();
         Command command = new GenericCommand();
         if (event.getMessage().getContentRaw().startsWith("!help")) {
-            if (getCommandBody(message).replaceAll(" ", "").length() > 5) {
+            if (getCommandBody(message).replaceAll(" ", "").length() > 5) { //what is the point of this
                 event.getChannel().sendTyping().queue();
-                getDescription();
+                getDescription(false);
             } else {
                 event.getChannel().sendTyping().queue();
-                helpSpecific(command, channel, false);
-            }
-        }
-    }
-    @Override
-    public void onSlashCommandInteraction (SlashCommandInteractionEvent event) {
-        Guild guild = event.getGuild();
-        TextChannel channel = event.getChannel().asTextChannel();
-        String content = event.getInteraction().getCommandString().replaceAll("/", "");
-        if (event.getName().equals("info")) {
-            try {
-                if (content.equals("help"))
-                    helpSpecific(new HelperBot(), channel, true);
-                if (content.equals("info"))
-                    helpSpecific(new InformationBot(), channel, true);
-                if (content.equals("play"))
-                    helpSpecific(new PlayBot(), channel, true);
-            } catch (Exception e) {
-                helpGeneral(event.getChannel().asTextChannel());
+                helpSpecific(command, channel, hook, false);
             }
         }
     }
 
-    private void helpSpecific(Command command, TextChannel channel, boolean isSlash) {
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        event.deferReply().queue(hook -> {
+            TextChannel channel = event.getChannel().asTextChannel();
+            String content = event.getInteraction().getCommandString().replaceAll("/", "");
+            if (event.getName().equals("help")) {
+                try {
+                    helpSpecific(new HelperBot(), channel, hook, true);
+                    if (content.equals("info"))
+                        helpSpecific(new InformationBot(), channel, hook, true);
+                    if (content.equals("play"))
+                        helpSpecific(new PlayBot(), channel, hook, true);
+                    if (content.equals("piss"))
+                        onFailure("willy", channel);
+                } catch (Exception e) {
+                    helpGeneral(event.getChannel().asTextChannel());
+                }
+            }
+        });
+    }
+
+    private void helpSpecific(Command command, TextChannel channel, InteractionHook hook, boolean isSlash) {
         EmbedBuilder embedBuilder = new EmbedBuilder();
         embedBuilder.setTitle("Command " + command.getIdentifier() + ":");
         String descriptionFormat = command.getDescription(isSlash);
@@ -92,7 +88,7 @@ public class HelperBot extends ListenerAdapter implements Command {
     @Override
     public void onFailure(String errorMessage, TextChannel channel) {
         LOGGER.error("HelperBot error. \n\n{}", errorMessage);
-        channel.sendMessage("Command Failed. Shape up man.").queue();
+        channel.sendMessage("Oh no! <@430373875549732874>! Help! It broke!").queue();
         //i should make this mention me since this means there's a pretty bad error
     }
 
@@ -129,16 +125,3 @@ public class HelperBot extends ListenerAdapter implements Command {
         return null; //check for a different way of doing this, probably GenericCommand.java having it's own way of getting the roles idk
     }
 }
-
-/*
-if (getCommandBody(message).equals("play"))
-            command = new PlayBot();
-        else if (getCommandBody(message).equals("help"))
-            command = new HelperBot();
-        else if (getCommandBody(message).equals("info") || getCommandBody(message).equals("information"))
-            command = new InformationBot();
-        else {
-                onFailure("No Such Command Exists", channel);
-                return;
-            }
- */
