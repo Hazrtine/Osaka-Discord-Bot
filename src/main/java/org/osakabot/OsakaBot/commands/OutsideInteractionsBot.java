f2be518ae2d1abd3645601c23ef6b1fd1abcb97a
Hazrtine <jamalvbadr@gmail.com>
2025-06-29 16:03:30 -0500

    wait no this is stage 1 what the hell did i change two commits ago?

--- src/main/java/org/osakabot/OsakaBot/commands/OutsideInteractionsBot.java
+++ src/main/java/org/osakabot/OsakaBot/commands/OutsideInteractionsBot.java
@@ -1,4 +1,79 @@
 package org.osakabot.OsakaBot.commands;
 
-public class OutsideInteractionsBot {
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.osakabot.OsakaBot.backend.Command;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class OutsideInteractionsBot extends ListenerAdapter implements Command {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OutsideInteractionsBot.class);
+
+    public OutsideInteractionsBot() {
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("server")) {
+            String action = event.getOption(event.getOptions().get(0).getAsString()).getAsString();
+            if (action.equals("anso")) {
+                event.reply("Please provide the username to add as server operator.").queue();
+            } else if (action.equals("tso")) {
+                event.reply("Turning the server on... 🔌🖥️").queue();
+                // trigger your startup logic
+            } else {
+                event.reply("...what?").setEphemeral(true).queue();
+            }
+        }
+    }
+
+    @Override
+    public void onSuccess() {
+        LOGGER.debug("{} Successful!", getClass());
+    }
+
+    public void onFailure(String errorMessage, TextChannel channel) {
+        LOGGER.debug("Error in InformationBot\n\n{}", errorMessage);
+        channel.sendMessage("Command Failed. Shape up man.").queue();
+    }
+
+    @Override
+    public boolean isFailed() {
+        LOGGER.error("InformationBot Failed!");
+        return false;
+    }
+
+    @Override
+    public String getCommandBody(Message message) {
+        return message.getContentRaw().replaceFirst(getIdentifier(), "").replaceAll(" ", "");
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "info";
+    }
+
+    @Override
+    public String display(Message message) {
+        return message.getContentRaw();
+    }
+
+    @Override
+    public String getDescription(boolean isSlash) {
+        return "Gets your information about any guild that you want. Stuff like owners, etc.";
+    }
+
+    @Override
+    public List<Role> permissionsList() {
+        //List<Role> roles = guild.getRoles();
+        //roles.removeIf(role -> !channel.getPermissionOverride(role).getAllowed().contains(Permission.MESSAGE_HISTORY));
+        //return roles;
+        return null;
+    }
 }
